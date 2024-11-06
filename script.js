458aa8c93697af4026b65094e683d80ee6c13138
Tony Paris <tony.parispro@gmail.com>
2024-11-06 10:10:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const eclipseContainer = document.querySelector(".eclipse-container");
+    eclipseContainer.classList.add("active");
+});
